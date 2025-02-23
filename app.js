04b8b99fd3a10f76821ed03b072432e6a1dbb6d7
Azeez <mohamedabdulazeez.25ec@licet.ac.in>
2025-02-23 20:48:17 +0530

    Updated message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('A demo DevOps Project.'));
+app.get('/', (req, res) => res.send('A demo DevOps Project. This is a change to the code.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
